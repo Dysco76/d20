5eb06f3ac568ad38e7622dbad4302ecfd61298d7
Dmytro Shcherba <dscherba76@gmail.com>
2023-04-26 14:49:30 +0300

    fix

--- src/D20Scene.js
+++ src/D20Scene.js
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
